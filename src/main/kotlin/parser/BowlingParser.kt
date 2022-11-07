0237e81d8afe539002e237743be66ef087471889
jSchoenaers <julien.schoenaers@gmail.com>
2022-11-08 00:01:25 +0100

    Completed the frame parsing function

--- src/main/kotlin/parser/BowlingParser.kt
+++ src/main/kotlin/parser/BowlingParser.kt
@@ -1,5 +1,6 @@
 package parser
 
+import models.Frame
 import models.Game
 
 class BowlingParser : GameParser {
@@ -10,8 +11,12 @@ class BowlingParser : GameParser {
         TODO("Not yet implemented")
     }
 
-    fun asFrame(frame: String) {
+    fun asFrame(frame: String) : Frame {
         require(allowedFrames.matches(frame)) { "Invalid frame '$frame'" }
+        val first = frame[0].toValue()
+        val second = frame.getOrNull(1)?.toValue(frame[0])
+        val third = frame.getOrNull(2)?.toValue(frame.getOrNull(1))
+        return Frame.fromThrows(first, second, third)
     }
 
     private fun Char.toValue(previous: Char? = null): Int =
