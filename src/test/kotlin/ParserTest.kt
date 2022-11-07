8fd826a8161c63fa81b664e45b541b3dd7823a9a
jSchoenaers <julien.schoenaers@gmail.com>
2022-11-08 00:01:01 +0100

    Added test cases for parsing frame

--- src/test/kotlin/ParserTest.kt
+++ src/test/kotlin/ParserTest.kt
@@ -1,3 +1,8 @@
+import models.Last
+import models.Open
+import models.Spare
+import models.Strike
+import org.junit.Assert
 import org.junit.Assert.assertThrows
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -7,20 +12,85 @@ class ParserTest {
 
         val parse = BowlingParser::asFrame
 
-        @Test
-        fun empty_string() {
-            val exception = assertThrows(IllegalArgumentException:: class.java){
-                parse(BowlingParser(),"")
-            }
-            assertTrue(exception.message !!.contains("Invalid frame ''"))
-        }
+    @Test
+    fun random_open() {
+        Assert.assertEquals(Open(2,5), parse(BowlingParser(), "25"))
+    }
 
-        @Test
-        fun too_long_string() {
-            val exception = assertThrows(IllegalArgumentException:: class.java){
-                parse(BowlingParser(),"5222")
-            }
-            assertTrue(exception.message !!.contains("Invalid frame '5222'"))
+    @Test
+    fun strike() {
+        Assert.assertEquals(Strike, parse(BowlingParser(), "X"))
+    }
+
+    @Test
+    fun spare_random() {
+        Assert.assertEquals(Spare(2), parse(BowlingParser(), "2/"))
+    }
+
+    @Test
+    fun spare_miss() {
+        Assert.assertEquals(Spare(0), parse(BowlingParser(), "-/"))
+    }
+
+    @Test
+    fun two_miss() {
+        Assert.assertEquals(Open(0,0), parse(BowlingParser(), "--"))
+    }
+
+    @Test
+    fun miss_open() {
+        Assert.assertEquals(Open(0,2), parse(BowlingParser(), "-2"))
+    }
+
+    @Test
+    fun open_miss() {
+        Assert.assertEquals(Open(2,0), parse(BowlingParser(), "2-"))
+    }
+
+    @Test
+    fun tenth_3_strike() {
+        Assert.assertEquals(Last(Strike,10,10), parse(BowlingParser(), "XXX"))
+    }
+
+    @Test
+    fun tenth_strike_spare() {
+        Assert.assertEquals(Last(Strike,1,9), parse(BowlingParser(), "X1/"))
+    }
+
+    @Test
+    fun tenth_strike_open() {
+        Assert.assertEquals(Last(Strike,1,2), parse(BowlingParser(), "X12"))
+    }
+
+    @Test
+    fun tenth_strike_open_miss() {
+        Assert.assertEquals(Last(Strike,1,0), parse(BowlingParser(), "X1-"))
+    }
+
+    @Test
+    fun tenth_spare_strike() {
+        Assert.assertEquals(Last(Spare(2),10,null), parse(BowlingParser(), "2/X"))
+    }
+
+    @Test
+    fun tenth_spare_open() {
+        Assert.assertEquals(Last(Spare(2),1,null), parse(BowlingParser(), "2/1"))
+    }
+    
+    @Test
+    fun empty_string() {
+        val exception = assertThrows(IllegalArgumentException:: class.java){
+            parse(BowlingParser(),"")
         }
+        assertTrue(exception.message !!.contains("Invalid frame ''"))
+    }
+
+    @Test
+    fun too_long_string() {
+        val exception = assertThrows(IllegalArgumentException:: class.java){
+            parse(BowlingParser(),"5222")
+        }
+        assertTrue(exception.message !!.contains("Invalid frame '5222'"))
+    }
 
 }
