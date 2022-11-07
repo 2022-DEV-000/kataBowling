932ee246340a178b0d2e02cadbc78ffe321ee611
jSchoenaers <julien.schoenaers@gmail.com>
2022-11-08 00:09:06 +0100

    Implement of parsar in main game in order to start scoring mechanic

--- src/main/kotlin/BowlingGame.kt
+++ src/main/kotlin/BowlingGame.kt
@@ -1,6 +1,13 @@
-class BowlingGame {
-    
-    infix fun totalScore(frames: String): Int {
-        return 0
-    }
+import models.Game
+import parser.BowlingParser
+import parser.GameParser
+
+class BowlingGame (
+    private val gameParser: GameParser = BowlingParser,
+
+    ) {
+        infix fun totalScore(frames: String): Int {
+            val game: Game = gameParser.parse(frames)
+            return 0
+        }
 }
